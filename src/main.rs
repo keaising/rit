48b9510d01158bdd422e5cdb04947dd790975833
keaising <keaising@gmail.com>
2021-05-29 18:53:00 +0800

    add subcommand and read content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{App, Arg};
+
+fn main() -> anyhow::Result<()> {
+    // let content = std::fs::read_to_string(&args.path)
+        // .with_context(|| format!("could not read file `{}`", path))?;
+        //
+
+    let matches = App::new("rit")
+        .version("0.1.0")
+        .author("keaising <keaising@gmail.com>")
+        .subcommand(
+            App::new("clone") 
+            .about("clone git repo")
+            .arg(
+                Arg::with_name("url")
+                .help("url need clone")
+                .required(true),
+                ),
+            )
+        .get_matches();
+
+    let msg = matches.subcommand_matches("clone")
+        .unwrap()
+        .value_of("url")
+        .unwrap();
+    println!("{}", msg);
+
+    Ok(())
 }
