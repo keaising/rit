f056dc78239a283212a923e49c2a548fba75f90f
keaising <keaising@gmail.com>
2021-05-30 11:35:49 +0800

    no panic when no cmds

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,38 @@ use clap::{App, Arg};
 
 fn main() -> anyhow::Result<()> {
     // let content = std::fs::read_to_string(&args.path)
-        // .with_context(|| format!("could not read file `{}`", path))?;
-        //
+    // .with_context(|| format!("could not read file `{}`", path))?;
+    //
 
     let matches = App::new("rit")
         .version("0.1.0")
         .author("keaising <keaising@gmail.com>")
         .subcommand(
-            App::new("clone") 
-            .about("clone git repo")
-            .arg(
-                Arg::with_name("url")
-                .help("url need clone")
-                .required(true),
-                ),
-            )
+            App::new("clone")
+                .about("clone git repo")
+                .arg(Arg::with_name("url").help("url need clone").required(true)),
+        )
+        .subcommand(
+            App::new("invoke")
+                .about("invoke website"),
+        )
         .get_matches();
 
-    let msg = matches.subcommand_matches("clone")
-        .unwrap()
-        .value_of("url")
-        .unwrap();
-    println!("{}", msg);
+    match matches.subcommand_matches("clone") {
+        Some(cmd) => {
+            match cmd.value_of("url") {
+                Some(url) => { println!("clone {}", url) }
+                None => {}
+            }
+        }
+        None => {}
+    }
+
+
+    match matches.subcommand_matches("invoke") {
+        Some(_) => { }
+        None => {}
+    }
 
     Ok(())
 }
